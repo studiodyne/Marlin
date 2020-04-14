8dd482ac465531012d99c24164f7d30b468b0f81
studiodyne <diabolomix@hotmail.fr>
2020-04-14 23:40:03 +0200

    Babystep xy

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1468,7 +1468,7 @@
 #if ENABLED(BABYSTEPPING)
   //#define INTEGRATED_BABYSTEPPING         // EXPERIMENTAL integration of babystepping into the Stepper ISR
   #define BABYSTEP_WITHOUT_HOMING
-  //#define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
+  #define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_INVERT_Z false         // Change if Z babysteps should go the other way
   #define BABYSTEP_MULTIPLICATOR_Z   40   // Babysteps are very small. Increase for faster motion.
 	#define BABYSTEP_MULTIPLICATOR_XY  1
