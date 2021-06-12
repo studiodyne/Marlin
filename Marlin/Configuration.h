1899c9603059742e5a57931baaf427ef28112e2a
studiodyne <diabolomix@hotmail.fr>
2021-06-12 22:36:20 +0200

    text 3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -138,7 +138,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Tronxy Init 2"
+#define CUSTOM_MACHINE_NAME "Tronxy Init 3"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
