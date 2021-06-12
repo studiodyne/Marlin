e89d8241e95672764c16bdfe614d83f3dba5b0f1
studiodyne <diabolomix@hotmail.fr>
2021-06-12 23:50:21 +0200

    No fade on LCD

--- Marlin/src/feature/leds/leds.cpp
+++ Marlin/src/feature/leds/leds.cpp
@@ -182,21 +182,43 @@ void LEDLights::set_color(const LEDColor &incol
         , new_w
       #endif
       ;
-      uint8_t old_r= leds.color.r,old_g= leds.color.g,old_b= leds.color.b,old_w= leds.color.w;
-      if (!leds.lights_on) {
-        old_r = old_g = old_b = old_w = 0;
-      }
-    for (int16_t i = 0; i < 256; i++) {
-      new_r= old_r + ( (i * (incol.r - old_r) ) / 255);
-      new_g= old_g + ( (i * (incol.g - old_g) ) / 255);
-      new_b= old_b + ( (i * (incol.b - old_b) ) / 255);
-      UPDATE_RGBW(R,new_r); UPDATE_RGBW(G,new_g); UPDATE_RGBW(B,new_b);
+    uint8_t old_r = leds.color.r,old_g = leds.color.g,old_b = leds.color.b
       #if ENABLED(RGBW_LED)
-        new_w= old_w + ( (i * (incol.w - old_w) ) / 255);
-        UPDATE_RGBW(W,new_w);
+        ,old_w = leds.color.w;
       #endif
-      delay(1);
-    }
+      ;
+      if (!leds.lights_on) {
+        old_r = old_g = old_b
+        #if ENABLED(RGBW_LED)
+          = old_w
+        #endif
+        = 0;
+      }
+      // To avoid fading loop when too little on LCD menu, and blocked screen
+      if (abs(incol.r - old_r) > 10 || abs(incol.g - old_g) > 10 || abs(incol.b - old_b) > 10
+       #if ENABLED(RGBW_LED)
+         || abs(incol.w - old_w) > 10
+       #endif
+         ) {
+        for (int16_t i = 0; i < 256; i++) {
+          new_r= old_r + ( (i * (incol.r - old_r) ) / 255);
+          new_g= old_g + ( (i * (incol.g - old_g) ) / 255);
+          new_b= old_b + ( (i * (incol.b - old_b) ) / 255);
+          UPDATE_RGBW(R,new_r); UPDATE_RGBW(G,new_g); UPDATE_RGBW(B,new_b);
+          #if ENABLED(RGBW_LED)
+            new_w= old_w + ( (i * (incol.w - old_w) ) / 255);
+            UPDATE_RGBW(W,new_w);
+          #endif
+          delay(1);
+        }
+      }
+      else
+      {
+        UPDATE_RGBW(R,incol.r); UPDATE_RGBW(G,incol.g); UPDATE_RGBW(B,incol.b);
+        #if ENABLED(RGBW_LED)
+          UPDATE_RGBW(W,incol.w);
+        #endif
+      }
 
   #endif
 
