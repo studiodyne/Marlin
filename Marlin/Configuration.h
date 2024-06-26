b05429face27b8ea60e9a1d120e1879269e6a75f
studiodyne <42887851+studiodyne@users.noreply.github.com>
2024-06-26 15:03:38 +0200

    V19.2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -116,7 +116,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MT1.5 V19MPC"
+#define CUSTOM_MACHINE_NAME "MT1.5 V19.2exp"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
