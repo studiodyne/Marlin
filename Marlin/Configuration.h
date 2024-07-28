272db75d1849ceeb463ea9828d662fc3edb931d1
studiodyne <diabolomix@hotmail.fr>
2024-07-28 12:52:57 +0200

    2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -126,7 +126,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Tronxydual V30"
+#define CUSTOM_MACHINE_NAME "Tronxydual V32"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
