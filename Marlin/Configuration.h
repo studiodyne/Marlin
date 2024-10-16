a5c3d397031c70fb00d51480c308201b2399281d
studiodyne <diabolomix@hotmail.fr>
2024-10-16 14:49:19 +0200

    MKS SCREEN + REPRAP KNOB

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2684,7 +2684,7 @@
 // This option overrides the default number of encoder pulses needed to
 // produce one step. Should be increased for high-resolution encoders.
 //
-#define ENCODER_PULSES_PER_STEP 2
+#define ENCODER_PULSES_PER_STEP 4
 
 //
 // Use this option to override the number of step signals required to
@@ -2707,7 +2707,7 @@
 //
 //  Set this option if CLOCKWISE causes values to DECREASE
 //
-//#define REVERSE_ENCODER_DIRECTION
+#define REVERSE_ENCODER_DIRECTION
 
 //
 // This option reverses the encoder direction for navigating LCD menus.
